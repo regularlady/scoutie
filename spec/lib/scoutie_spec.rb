d7392aa2efdb1d1ea7e621f7c8c62b0cd63e0767
Brittany Martin <brittany.jill.martin@gmail.com>
2015-02-25 20:47:00 -0800

    Added Class to Status and adjusted specs

--- spec/lib/scoutie_spec.rb
+++ spec/lib/scoutie_spec.rb
@@ -1,26 +1,28 @@
 require 'spec_helper'
 
-describe Scoutie do
+module Scoutie 
+  describe Status do
+    describe "#get_status" do
+      let(:status) { Status.new.get_status }
 
-  it "returns a 200 status code when available" do
-    puts "blahblah"
-    puts blah
-    status = get_status 'http://www.telize.com/geoip\?callback\=\?'
-    expect(status).to be(200)
+      it "returns a 200 status code when available" do
+        status = get_status 'http://www.telize.com/geoip\?callback\=\?'
+        expect(status).to be(200)
+      end
+
+      it "returns a 403 status code when denied" do
+        status = get_status 'http://lorem-ipsum.me/api/json'
+        expect(status).to be(403)
+      end
+
+      it "returns a 404 status code when denied" do
+        status = get_status 'http://content.guardianapis.com/.json'
+        expect(status).to be(404)
+      end
+
+      it "throws exception if no protocol provided" do
+        expect{get_status 'bananas!'}.to raise_error(URI::InvalidURIError)
+      end
+    end
   end
-
-  # it "returns a 403 status code when denied" do
-  #   status = get_status 'http://lorem-ipsum.me/api/json'
-  #   expect(status).to be(403)
-  # end
-
-  # it "returns a 404 status code when denied" do
-  #   status = get_status 'http://content.guardianapis.com/.json'
-  #   expect(status).to be(404)
-  # end
-
-  # it "throws exception if no protocol provided" do
-  #   expect{get_status 'bananas!'}.to raise_error(URI::InvalidURIError)
-  # end
-
 end
